a3741596f36fe186a76aeb9e535a13567a01752f
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-08-06 07:12:28 +0800

    Update GameCircle.java

--- com/clay/ge/shape/GameCircle.java
+++ com/clay/ge/shape/GameCircle.java
@@ -1,9 +1,6 @@
 package com.clay.ge.shape;
 
-import com.clay.ge.render.GameBounds;
-import com.clay.ge.render.GameRender;
-import com.clay.ge.render.GameShape;
-import com.clay.ge.render.GameShapeType;
+import com.clay.ge.render.*;
 
 import java.awt.*;
 
@@ -11,6 +8,10 @@ public class GameCircle extends GameShape {
     private float x, y, radius;
 
     public GameCircle() {}
+
+    public GameCircle(GameColor color) {
+        setColor(color);
+    }
 
     public GameCircle(float x, float y, float radius) {
         this.x = x;
@@ -56,14 +57,67 @@ public class GameCircle extends GameShape {
         Graphics2D g2 = render.getGraphics();
         g2.setRenderingHints(getRenderingHints());
         g2.setColor(getColor().toAWTColor());
-        g2.setStroke(stroke());
-        if (getType().equals(GameShapeType.Fill)) g2.fillOval((int) x, (int) y, (int) radius, (int) radius);
-        else g2.drawOval((int) x, (int) y, (int) radius, (int) radius);
-    }
+        if (getType().equals(GameShapeType.Fill)) {
+            g2.fillOval((int) x, (int) y, (int) radius, (int) radius);
+            g2.setStroke(strokeFill());
+            g2.setColor(getStrokeColor().toAWTColor());
+            g2.drawOval((int) x, (int) y, (int) radius, (int) radius);
+        } else {
+            g2.setStroke(strokeDraw());
+            g2.drawOval((int) x, (int) y, (int) radius, (int) radius);
+        }
 
-    @Override
-    public Rectangle getBounds() {
-        return new Rectangle((int) x, (int) y, (int) radius, (int) radius);
+        g2.setColor(Color.YELLOW);
+        g2.setStroke(new BasicStroke(
+                0.1f,
+                BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER,
+                5.f,
+                new float[] {2.5f, 2.5f},
+                0.f
+        ));
+
+        int xx = (int) (x - getStroke() / 2) - 2;
+        int yy = (int) (y - getStroke() / 2) - 2;
+        int xw = (int) (radius + getStroke()) + 4;
+        int yh = (int) (radius + getStroke()) + 4;
+
+        //setBounds(new Rectangle(xx, yy, xw, yh));
+
+        int r = (int) radius / 3;
+        Point a = new Point(xx + r, yy);
+        Point b = new Point(xx + xw - r, yy);
+        Point c = new Point(xx + xw, yy + r);
+        Point d = new Point(xx + xw, yy + yh - r);
+        Point e = new Point(xx + xw - r, yy + yh);
+        Point f = new Point(xx + r, yy + yh);
+        Point g = new Point(xx, yy + yh - r);
+        Point h = new Point(xx, yy + r);
+
+        GameBounds bounds = new GameBounds();
+
+        bounds.add(a);
+        bounds.add(b);
+        bounds.add(c);
+        bounds.add(d);
+        bounds.add(e);
+        bounds.add(f);
+        bounds.add(g);
+        bounds.add(h);
+
+        setBounds(bounds);
+
+        if (getBorderLine()) {
+            g2.drawLine(a.x, a.y, b.x, b.y);
+            g2.drawLine(b.x, b.y, c.x, c.y);
+            g2.drawLine(c.x, c.y, d.x, d.y);
+            g2.drawLine(d.x, d.y, e.x, e.y);
+            g2.drawLine(e.x, e.y, f.x, f.y);
+            g2.drawLine(f.x, f.y, g.x, g.y);
+            g2.drawLine(g.x, g.y, h.x, h.y);
+            g2.drawLine(h.x, h.y, a.x, a.y);
+        }
+
     }
 
 }
