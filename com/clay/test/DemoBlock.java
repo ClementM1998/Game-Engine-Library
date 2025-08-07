38433698f0728f02a607cc66bc75bb4730f6a5d7
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-08-07 18:39:18 +0800

    Update DemoBlock.java

--- com/clay/test/DemoBlock.java
+++ com/clay/test/DemoBlock.java
@@ -15,6 +15,7 @@ public class DemoBlock extends GameApplication {
     int left, top, right, bottom;
 
     GamePoint point = new GamePoint();
+
     GameRectangle background = new GameRectangle();
     GameCircle player = new GameCircle();
 
@@ -25,5 +26,55 @@ public class DemoBlock extends GameApplication {
 
         width = GetWindowWidth() - radius;
         height = GetWindowHeight() - radius;
-        radius = 
+
+        radius = (width / 3 - height / 3) / 4;
+        if (radius < y) radius = y;
+
+        left = x;
+        top = y;
+        right = (width - left);
+        bottom = (height - top);
+        background.set(left, top, right - x + radius, bottom - y + radius);
+
+        player.set(mx, my, radius);
+
+        int px = width / 2;
+        int py = height / 2;
+        point.set(px, py);
+    }
+
+    @Override
+    public void update(GameEvent event) {
+
+        if (IsKeyPressed(GameKeys.GK_ESCAPE)) CloseWindow();
+
+        if (IsKeyPressed(GameKeys.GK_LEFT)) mx -= 6;
+        if (IsKeyPressed(GameKeys.GK_RIGHT)) mx += 6;
+
+        if (IsKeyPressed(GameKeys.GK_UP)) my -= 6;
+        if (IsKeyPressed(GameKeys.GK_DOWN)) my += 6;
+
+        if (mx < left) mx = left;
+        if (mx > right) mx = right;
+        if (my < top) my = top;
+        if (my > bottom) my = bottom;
+
+        player.setX(mx);
+        player.setY(my);
+    }
+
+    @Override
+    public void render(GameRender render) {
+        ClearWindow();
+        background.draw(render);
+
+        player.draw(render);
+
+        point.draw(render);
+    }
+
+    public static void main(String[] args) {
+        launch(new DemoBlock());
+    }
+
 }
