170591321f9a8fdc1a35b5d49a1b1f8f00bfa6b5
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-08-07 18:35:05 +0800

    Create DemoBlock.java

---
+++ com/clay/test/DemoBlock.java
@@ -0,0 +1,12 @@
+package com.clay.test;
+
+import com.clay.ge.GameApplication;
+import com.clay.ge.io.GameEvent;
+import com.clay.ge.io.GameKeys;
+import com.clay.ge.render.GameRender;
+import com.clay.ge.shape.GameCircle;
+import com.clay.ge.shape.GamePoint;
+import com.clay.ge.shape.GameRectangle;
+import com.clay.ge.ui.GameWindow;
+
+public class DemoBlock extends 
