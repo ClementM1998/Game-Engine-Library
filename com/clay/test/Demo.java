7345d6bfaf3ab8083b488c1d4c9818b6761e2174
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-08-07 19:19:25 +0800

    Update Demo.java

--- com/clay/test/Demo.java
+++ com/clay/test/Demo.java
@@ -1,124 +1,41 @@
 package com.clay.test;
 
 import com.clay.ge.GameApplication;
-import com.clay.ge.render.GameColor;
-import com.clay.ge.shape.GameCircle;
-import com.clay.ge.shape.GameRectangle;
 import com.clay.ge.shape.GameText;
-import com.clay.ge.shape.GameTriangle;
 import com.clay.ge.ui.GameWindow;
 import com.clay.ge.io.GameEvent;
 import com.clay.ge.io.GameKeys;
 import com.clay.ge.render.GameRender;
-import com.clay.ge.util.GameCollision;
+
+import java.util.ArrayList;
 
 public class Demo extends GameApplication {
-    private GameCircle circle = new GameCircle();
-    private GameCircle circle1 = new GameCircle();
-    private GameRectangle rect = new GameRectangle();
-    private GameText text = new GameText();
-    private GameTriangle triangle = new GameTriangle();
-    private float cx = 200, cy = 200, radius = 50;
-    private float width, height;
-    private boolean collision = false;
-    private float[] xpoint = {300, 325, 350}, ypoint = {150, 100, 150};
+    private ArrayList<GameText> arrayText = new ArrayList<>();
 
     @Override
     public void init(GameWindow window) {
-        CreateWindow("Demo", true);
-        width = GetWindowWidth() - radius;
-        height = GetWindowHeight() - radius;
+        CreateWindow("Demo");
 
-        text.set("0x0", 0, 0);
+        arrayText.add(new GameText("Welcome to Game Engine Library", 10, 10));
+        arrayText.add(new GameText("  > Press 'Esc' to exit", 10, 30));
 
-        circle.set(cx, cy, radius);
-        circle.setAntialias(true);
-        //circle.setType(GameShapeType.Fill);
-        //circle.setStroke(3.5f);
-
-        circle1.set(300, 300, 100);
-
-
-        rect.set(100, 200, 50, 50);
-        rect.setColor(GameColor.BLUE);
-        //rect.setType(GameShapeType.Fill);
-
-        triangle.set(xpoint, ypoint);
-        triangle.setColor(GameColor.BLUE);
+        for (GameText text : arrayText) {
+            text.setSize(15);
+        }
     }
 
     @Override
     public void update(GameEvent event) {
-        if (IsKeyPressed(GameKeys.GK_LEFT)) cx -= 5;
-        if (IsKeyPressed(GameKeys.GK_UP)) cy -= 5;
-        if (IsKeyPressed(GameKeys.GK_RIGHT)) cx += 5;
-        if (IsKeyPressed(GameKeys.GK_DOWN)) cy += 5;
         if (IsKeyPressed(GameKeys.GK_ESCAPE)) CloseWindow();
-        if (IsMouseButtonLeftPressed()) System.out.println("Mouse Button Left : " + IsMouseButtonLeftPressed());
-        if (IsMouseButtonMiddlePressed()) CloseWindow();
-        if (IsMouseButtonRightPressed()) System.out.println("Mouse Button Right : " + IsMouseButtonRightPressed());
-
-        if (cx > 0 && cy > 0 && cx < width && cy < height) {
-            collision = false;
-        } else {
-            if (cx <= 0) {
-                cx = 0;
-                collision = true;
-            }
-            if (cy <= 0) {
-                cy = 0;
-                collision = true;
-            }
-            if (cx >= width) {
-                cx = width;
-                collision = true;
-            }
-            if (cy >= height) {
-                cy = height;
-                collision = true;
-            }
-        }
-
-
-        if (collision) circle.setColor(GameColor.BROWN);
-        else circle.setColor(GameColor.WHITE);
-
-        if (GameCollision.collision(circle, rect)) {
-            circle.setColor(GameColor.BLUE);
-            rect.setColor(GameColor.BLUE);
-        } else {
-            rect.setColor(GameColor.GREEN);
-        }
-
-        if (GameCollision.collision(circle, triangle)) {
-            triangle.setColor(GameColor.BLUE);
-        } else {
-            triangle.setColor(GameColor.GREEN);
-        }
-
-        if (GameCollision.collision(circle, circle1)) {
-            circle1.setColor(GameColor.WHITE);
-        } else {
-            circle1.setColor(GameColor.BROWN);
-        }
-
-        text.setText("Move : " + (int) cx + "x" + (int) cy);
-
-        circle.setX(cx);
-        circle.setY(cy);
-        //rect.setLeft(cx);
-        //rect.setTop(cy);
     }
 
     @Override
     public void render(GameRender render) {
         ClearWindow();
 
-        text.draw(render);
-        circle.draw(render);
-        rect.draw(render);
-        triangle.draw(render);
-        circle1.draw(render);
+        if (arrayText != null) {
+            for (GameText text : arrayText) text.draw(render);
+        }
     }
 
     public static void main(String[] args) {
