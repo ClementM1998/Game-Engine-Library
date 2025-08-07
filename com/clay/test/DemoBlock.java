47d7e8552577f98409fcd41648cc4f65afa7f1d2
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-08-07 18:37:39 +0800

    Update DemoBlock.java

--- com/clay/test/DemoBlock.java
+++ com/clay/test/DemoBlock.java
@@ -9,4 +9,21 @@ import com.clay.ge.shape.GamePoint;
 import com.clay.ge.shape.GameRectangle;
 import com.clay.ge.ui.GameWindow;
 
-public class DemoBlock extends 
+public class DemoBlock extends GameApplication {
+    int x = 100, y = 60, width, height;
+    int mx = 0, my = 0, radius = 50;
+    int left, top, right, bottom;
+
+    GamePoint point = new GamePoint();
+    GameRectangle background = new GameRectangle();
+    GameCircle player = new GameCircle();
+
+    @Override
+    public void init(GameWindow window) {
+        CreateWindow("Demo Block", true);
+        ShowTitleFPS();
+
+        width = GetWindowWidth() - radius;
+        height = GetWindowHeight() - radius;
+        radius = 
+}
